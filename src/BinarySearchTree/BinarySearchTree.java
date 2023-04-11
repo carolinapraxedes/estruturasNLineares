8fe06ef84eaf5c1232abb77c0ebfe69867d43890
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-04-11 20:15:32 -0300

    arvore funcionando e mostrando corretamente

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 
 public class BinarySearchTree {	
-	   Node root;
+	 Node root;
 	   int size;
 	   ArrayList<Node> ArrayNo = new ArrayList<>();
 
@@ -74,20 +74,6 @@ public class BinarySearchTree {
 
 	        return newNode;
 	    
-	    	/*Node no = search(root,key);
-	    	if(no.getKey()== key) {
-	    		 throw new NodeExistsException("Nó já existe");
-	    	}else{
-	    		Node newNode = new Node(key);
-	    		newNode.setParent(no);
-	    		if(newNode.isRightChild()) {
-	    			no.setRightChild(newNode);
-	    		}else {
-	    			no.setLeftChild(newNode);
-	    		}
-	    		
-	    		return newNode;
-	    	}*/
 	    }
 	    
 	    
@@ -128,45 +114,6 @@ public class BinarySearchTree {
 	        }
 	        return removedKey;
 	        
-	    	/*Node no = search(root,key);
-	    	Node noParent = no.getParent();
-	    	key = no.getKey();
-	    	Node child = null;
-	    	if(no.getKey() == 0) {
-	    		 throw new NodeExistsException("Nó não existe");
-	    	}
-	    	if(isExternal(no)) {
-	    		key = no.getKey();	    			    		
-	    		if (no.isRightChild()) {
-	    			noParent.setRightChild(null);
-	    		}else {
-	    			noParent.setLeftChild(null);
-	    		}
-	    		return key;
-	    	}
-	    	if (hasOneChild(no)) {
-	    		if(no.hasLeftChild(no)) {
-	    			child = no.getLeftChild();
-	    		}else {
-	    			child = no.getRightChild();
-	    		}
-	    		if(no.hasRightChild(no)) {
-	    			noParent.setRightChild(child);
-	    		}else {
-	    			noParent.setLeftChild(child);
-	    		}	    		
-	    		child.setParent(noParent);
-	    		return key;
-	    	}
-			
-			if(hasTwoChild(no)) {
-				key = no.getKey();
-				Node successor = successor(no.getRightChild());
-				remove(successor.getKey());
-				key = successor.getKey();
-				return key;
-			}
-			return key;*/
 	    }
 	    
 	    public boolean hasOneChild(Node no) {
@@ -197,13 +144,33 @@ public class BinarySearchTree {
 		       if(node.getLeftChild() != null) {
 		    	   print(node.getLeftChild());
 		       }
-		       System.out.print(node.getKey()+" ");
+	
+		       ArrayNo.add(node);
 		       
 		       if(node.getRightChild()!=null) {
 		    	   
 		    	   print(node.getRightChild());
 		       }
+		       
 		    }
+	    
+	    public void printTree() {
+	    	ArrayNo.clear();
+	    	print(getRoot());
+	    	Object tree[][] = new Object[height(getRoot()) +1 ][ArrayNo.size()];
+	    	
+	    	for(int i = 0; i<ArrayNo.size();i++) {
+	    		tree[depth(ArrayNo.get(i))][i] = ArrayNo.get(i).getKey();
+	    	}
+	    	
+	    	for(int l = 0;l<height(getRoot())+1;l++) {
+	    		for(int c =0; c < ArrayNo.size();c++) {
+	    			System.out.print(tree[l][c] != null ? tree[l][c]+"\t" : "\t");
+	    		}
+	    		System.out.println("");
+	    	}
+	    }
+	    
 	    
 	
 	    public Node getRoot() {
@@ -232,72 +199,7 @@ public class BinarySearchTree {
 			return 1 + (depth(no.getParent()));
 		}
 
-	    
- 	    /*public int depth(int key) {
-	        return depthNode(root, key, 0);
-	    }
 
-	    private int depthNode(Node node, int key, int depth) {
-	        if (node == null) {
-	            return -1;
-	        } else if (key == node.key) {
-	            return depth;
-	        } else if (key < node.key) {
-	            return depthNode(node.left, key, depth + 1);
-	        } else {
-	            return depthNode(node.right, key, depth + 1);
-	        }
-	    }
-
-	    public int height(Node node) {
-	        if (node == null) {
-	        	//altura vai ser zero
-	            return -1;
-	        }
-
-	        //alturas dos filhos do nó passado
-	        int leftHeight = height(node.left);
-	        int rightHeight = height(node.right);
-
-	        //a altura do nó passado vai ser a soma de 1 mais a altura da subárvore mais alta entre seus filhos
-	        return 1 + Math.max(leftHeight, rightHeight);
-	    }*/
-	    
-
-	    /*void insert(int key) {
-	        root = insertNode(root, key);
-	    }
-
-	    Node insertNode(Node root, int key) {
-	    		    	
-	        if (root == null) {
-	            root = new Node(key);
-	            return root;
-	        }
-
-	        if (key < root.key) {
-	        	//se for menor que a root, vai pra esquerda
-	            root.left = insertNode(root.left, key);
-	        } else if (key > root.key) {
-	        	//se for maior que a root, vai pra direita
-	            root.right = insertNode(root.right, key);
-	        }
-
-	        return root;
-	    }
-			*/
-
-	    /*public void inOrder(Node node) {
-	       if(node.getLeftChild() != null) {
-	    	   inOrder(node.getLeftChild());
-	       }
-	       
-	       
-	       if(node.getRightChild()!=null) {
-	    	   
-	    	   inOrder(node.getRightChild());
-	       }
-	    }*/
 
 
 }
--- src/BinarySearchTree/BinarySearchTreeTest.java
+++ src/BinarySearchTree/BinarySearchTreeTest.java
@@ -4,27 +4,27 @@ public class BinarySearchTreeTest {
 
 	public static void main(String[] args) throws NodeExistsException {
 		 BinarySearchTree tree = new BinarySearchTree(9);
-		 	tree.insert(8);
+		 tree.insert(8);
 		 	tree.insert(10);
 		 	tree.insert(5);
 		 	tree.insert(6);
-		 	tree.print(tree.getRoot());
+		 	tree.printTree();
 		 	tree.remove(5);
 		 	System.out.println();
-		 	tree.print(tree.getRoot());
+		 	tree.printTree();
 		 	
 		 	
 		 	tree.insert(4);
 		 	System.out.println();
-		 	tree.print(tree.getRoot());
+		 	tree.printTree();
 		 	
 		 	tree.insert(11);
 		 	System.out.println();
-		 	tree.print(tree.getRoot());
+		 	tree.printTree();
 		 	
 		 	tree.remove(10);
 		 	System.out.println();
-		 	tree.print(tree.getRoot());
+		 	tree.printTree();
 		 	
 	}
 
