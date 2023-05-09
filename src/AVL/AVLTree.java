4da28dcd1eeaee6995d1afaac066afef8a4015c3
Carolina Praxedes <ana_carolina47@hotmail.com>
2023-05-08 21:28:01 -0300

    arvore nao girando

--- src/AVL/AVLTree.java
+++ src/AVL/AVLTree.java
@@ -96,6 +96,7 @@ public class AVLTree{
         }
 
         Node newNode = new Node(key);
+        checkBalance(newNode);
         newNode.setParent(parent);
 
         if (key < parent.getKey()) {
@@ -103,13 +104,162 @@ public class AVLTree{
         } else {
             parent.setRightChild(newNode);
         }
-        // Atualizar o fator de balanceamento ao longo do caminho percorrido
+        /*// Atualizar o fator de balanceamento ao longo do caminho percorrido
         int FB = getBalanceFactor(newNode);
-        newNode.setFB(FB);
+        newNode.setFB(FB);*/
 
         return newNode;
     }
     
+
+    
+    /*
+     * Métodos da árvore AVL
+     * */
+    
+
+    
+    public int getBalanceFactor(Node node) {
+        int heightLeft = height(node.getLeftChild());
+        int heightRight = height(node.getRightChild());
+        return heightLeft - heightRight;
+    }
+    
+    public void checkBalance(Node node) {
+        if (node == null) {
+            return;
+        }
+        
+        int balanceFactor = getBalanceFactor(node);
+        
+        if (balanceFactor > 1 || balanceFactor < -1) {
+            // A árvore está desbalanceada, fazer a rotação correspondente
+            if (balanceFactor > 1) {
+                if (getBalanceFactor(node.getLeftChild()) >= 0) {
+                    // Rotação simples à direita
+                	rotateRight(node);
+                	
+                } else {
+                    // Rotação dupla à esquerda-direita
+                    //rotationLeftRight(node);
+                	rotationLeftDouble(node);
+                	
+                }
+            } else {
+                if (getBalanceFactor(node.getRightChild()) <= 0) {
+                    // Rotação simples à esquerda
+                	rotateLeft(node);
+                	
+                	
+                } else {
+                    // Rotação dupla à direita
+                    rotationRightDouble(node);
+                    
+                	
+                }
+            }
+            
+            // Depois de fazer a rotação, é preciso atualizar os fatores de balanceamento dos nós afetados
+            if (node.getParent() != null) {
+                checkBalance(node.getParent());
+            } else {
+                root = node;
+            }
+        } else {
+            checkBalance(node.getParent());
+        }
+    }
+
+    
+ 
+    /*public Node balance(Node no) {
+    	Node noParent = no.getParent();
+    	Node noBalanced = null;
+    	
+    	int FB=0;
+    	if((no.getFB()<=1) && (no.getFB()>=-1)) {
+    		//está balanceado
+    		return no;
+    	}else {
+    		//fazendo o balanceamento
+    		Node child;
+    		if(noParent !=null) {
+    			//se ele não for raiz
+    			if(no.getFB()== -2) { //rotação a esquerda
+    				child= no.getRightChild();
+    				FB=no.getRightChild().getFB();
+    				
+    				if(FB<=0) { 
+    					//rotação simples
+    					rotationLeftSimple(no);
+    				}else if(FB>0) {
+    					//rotação dupla
+    				}
+    			}        			
+    		}
+    	}
+    	
+    	return  noBalanced;
+    	
+    }
+    */
+    
+	private Node rotateRight(Node node) {
+		 Node leftChild = node.getLeftChild();
+		    leftChild.setParent(node.getParent());
+
+		    node.setLeftChild(leftChild.getRightChild());
+
+		    if (node.getLeftChild() != null) {
+		        node.getLeftChild().setParent(node);
+		    }
+
+		    leftChild.setRightChild(node);
+		    node.setParent(leftChild);
+
+		    return leftChild;
+		
+	}
+    
+	public Node rotateLeft(Node node) { //ESQUERDA
+		 System.out.println("Aplicando rotação à esquerda simples em nó " + node.getKey());
+		    
+		    Node child = node.getRightChild();
+		    
+		    child.setParent(node.getParent());
+		    child.setLeftChild(node);
+		    
+		    node.setParent(child);
+		    
+		    int FBNo = getBalanceFactor(node);
+		    int FBChild = getBalanceFactor(child);
+		    node.setFB(FBNo);
+		    child.setFB(FBChild);
+		    
+		    System.out.println("Rotação à esquerda simples aplicada em nó " + node.getKey());
+		    
+		    return child;
+    }
+    
+
+    private Node rotationRightDouble(Node node) {
+    	 node.setRightChild(rotateRight(node.getRightChild()));
+    	    return rotateLeft(node);
+		
+	}
+
+	private Node rotationLeftDouble(Node node) {
+		  node.setLeftChild(rotateLeft(node.getLeftChild()));
+		    return rotateRight(node);
+		
+	}
+
+
+
+
+
+
+    /*Imprimir arvore*/
     public void inOrder(Node node) {
     	if (node == null) {
             return;
@@ -148,70 +298,6 @@ public class AVLTree{
     		System.out.println("");
     	}
     }
-    
-    /*
-     * Métodos da árvore AVL
-     * */
-    
-
-    
-    public int getBalanceFactor(Node node) {
-        int heightLeft = height(node.getLeftChild());
-        int heightRight = height(node.getRightChild());
-        return heightLeft - heightRight;
-    }
-    
-    public Node balance(Node no) {
-    	Node noParent = no.getParent();
-    	Node noBalanced = null;
-    	
-    	int FB=0;
-    	if((no.getFB()<=1) && (no.getFB()>=-1)) {
-    		//está balanceado
-    		return no;
-    	}else {
-    		//fazendo o balanceamento
-    		Node child;
-    		if(noParent !=null) {
-    			//se ele não for raiz
-    			if(no.getFB()== -2) { //rotação a esquerda
-    				child= no.getRightChild();
-    				FB=no.getRightChild().getFB();
-    				
-    				if(FB<=0) { 
-    					//rotação simples
-    					rotationLeftSimple(no);
-    				}else if(FB>0) {
-    					//rotação dupla
-    				}
-    			}        			
-    		}
-    	}
-    	
-    	return  noBalanced;
-    	
-    }
-    
-
-    public Node rotationLeftSimple(Node no) { //ESQUERDA
-    	
-    	Node child = no.getRightChild(); // filho
-    	
-    	child.setParent(no.getParent()); //o novo pai vai ser o avô
-    	child.setLeftChild(no);// pai vai ser filho esquerdo
-    	
-    	no.setParent(child);// o pai vai ser o filho
-    	
-    	//atualizando o FB
-    	 int fbNo = no.getFB();
-    	 int fbChild = child.getFB();
-    	 no.setFB(fbNo - 1 - Math.max(0, fbChild));
-    	 child.setFB(fbChild - 1 + Math.min(0, no.getFB()));
-    	
-    	return child;
-    }
-
-
   
     
     
--- src/AVL/Main.java
+++ src/AVL/Main.java
@@ -7,14 +7,26 @@ public class Main {
 
 	public static void main(String[] args) throws NodeExistsException {
 		 AVLTree tree = new AVLTree(9);
+		 	tree.printTree();
+		 	System.out.println("====================");
 		 	tree.insert(8);
+		 	tree.printTree();
+		 	System.out.println("====================");
 		 	tree.insert(10);
+		 	tree.printTree();
+		 	System.out.println("====================");
 
 		 	tree.insert(5);
-		 	tree.insert(6);
-//		 	tree.insert(7);
-		 	
 		 	tree.printTree();
+		 	System.out.println("====================");
+		 	tree.insert(6);
+		 	tree.printTree();
+		 	System.out.println("====================");
+		 	tree.insert(7);
+		 	tree.printTree();
+		 	System.out.println("====================");
+		 	
+		 	//tree.printTree();
 		 	//System.out.println(tree.getBalanceFactor(tree.getRoot()));
 
 
